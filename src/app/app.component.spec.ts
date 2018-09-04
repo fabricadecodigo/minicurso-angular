3676d39e62629b7e8bbaf9601071d0ff8e1907f1
Fábrica de Código <feliperfranco@gmail.com>
2018-09-04 11:18:58 -0300

    mais ajustes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'my-todo-app'`, async(() => {
+  it(`should have as title 'minicurso-angular'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-todo-app');
+    expect(app.title).toEqual('minicurso-angular');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-todo-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to minicurso-angular!');
   }));
 });
